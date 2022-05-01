96d495131dccfafc171c133884ef9ebcc9544b68
Krishan Gharu <kslg257@gmail.com>
2022-05-01 15:54:19 +0000

    Adjust validation to check that all values proivded are integers

--- run.py
+++ run.py
@@ -49,17 +49,21 @@ def validate_data(values):
     Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
+    
     For the except statement, here we will except ValueError as e: The ValueError class here contains the  
     details of the error triggered by the code in our  try statement here, and by using the as keyword,  
     we're assigning that ValueError object to the e variable, which is standard Python shorthand for “error”.
+   
+    Use List Comprehension to convert strings into integers:
+    For each individual value in the values  list, convert that value into an integer.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-
 
 get_sales_data()
